0da3fde7db343bb6c7573277c463cdf83bd416bb
majkon8 <48221693+majkon8@users.noreply.github.com>
2020-07-05 23:34:24 +0200

    Deploy to Heroku

--- connect.php
+++ connect.php
@@ -6,6 +6,8 @@ try {
     $connection = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
     if ($connection->connect_errno != 0) {
         throw new Exception("Connection to database error.");
+    } else {
+        echo "Jest gites";
     }
 } catch (Exception $error) {
     echo $error->getMessage();
